0c2e4ab3a78688a0a300eb16d0c17241a4d6c704
kisa22 <irvanagamingyt@gmail.com>
2024-05-22 14:19:49 +0700

    clear Project section

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,14 +1,12 @@
 import SectionHeading from "@/components/SectionHeading";
 import { projectsData } from "@/libs/data";
-import Image from "next/image";
 import React from "react";
+import Project from "@/components/Project";
 
 const Projects = () => {
   return (
-    <section className="text-color-primary">
-      <div className="flex text-center justify-center items-center">
-        <SectionHeading>Projects</SectionHeading>
-      </div>
+    <section className="text-color-primary mb-28">
+      <SectionHeading>My Projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
           <React.Fragment key={index}>
@@ -16,35 +14,6 @@ const Projects = () => {
           </React.Fragment>
         ))}
       </div>
-    </section>
-  );
-};
-
-type ProjectProps = (typeof projectsData)[number];
-
-const Project = ({ title, description, tags, imageUrl }: ProjectProps) => {
-  return (
-    <section className="bg-color-gray-100 max-w-[42rem] border border-color-darker/5 overflow-hidden sm:pr-8 text-color-darker relative sm:h-[22rem] mb-3 sm:mb-8 last:mb-0 rounded-sm shadow-xl">
-      <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full">
-        <h3 className="text-2xl font-semibold">{title}</h3>
-        <p className="mt-2 leading-relaxed">{description}</p>
-        <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
-          {tags.map((tag, index) => (
-            <li
-              key={index}
-              className="bg-color-darker/[0.8] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-color-primary rounded-full">
-              {tag}
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      <Image
-        src={imageUrl}
-        alt={title}
-        quality={95}
-        className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl"
-      />
     </section>
   );
 };
